797860b634b5047c1907c8bc561e78f83941d15b
master <666Lilly888@gmail.com>
2023-10-26 04:06:10 +0300

    added intersection observer

--- js/main.js
+++ js/main.js
@@ -633,3 +633,24 @@ if (delivery) {
   }
 }
 /* *************************************** */
+/* INTERSECTION OBSERVER */
+const header = document.querySelector("header");
+
+if (header) {
+  const fadeUpObserver = new IntersectionObserver(
+    (elsToWatch) => {
+      elsToWatch.forEach((el) => {
+        if (el.isIntersecting) {
+          el.target.classList.add("faded");
+          fadeUpObserver.unobserve(el.target);
+        }
+      });
+    },
+    { threshold: 0.25 }
+  );
+
+  document.querySelectorAll(".fade-up").forEach((item) => {
+    fadeUpObserver.observe(item);
+  });
+}
+/* END OF INTERSECTION OBSERVER */
